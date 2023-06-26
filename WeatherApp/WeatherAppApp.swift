0f6e7b806efcbec4d7bab22cc14025a2d3930c10
Matheus de Sousa Matos <69324938+Matheus-Sousa-Matos@users.noreply.github.com>
2023-06-26 20:13:34 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Matheus de Sousa Matos on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
